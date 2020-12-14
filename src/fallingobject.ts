f0f5c917b68d56969bdf36a1e365415ecbd35c59
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-12-14 13:19:32 +0100

    added class fallingobject

---
+++ src/fallingobject.ts
@@ -0,0 +1,40 @@
+import p5 from "p5"
+
+class FallingObject {
+
+    public size: number
+    public color: string
+    public soundeffect: boolean
+    public img: string
+    public position: p5.Vector
+    public speed: p5.Vector
+
+    constructor() {
+
+    }
+
+    update() {
+
+    }
+
+    draw() {
+
+    }
+
+    fallen() {
+    // fallande function
+    }
+
+    getPoints() {
+    // spelaren får eller förlorar poäng vid träff
+    }
+
+    random() {
+    // stjärnorna faller random
+    }
+
+    loop() {
+    // fallande objekten ska loopas och startas om o om igen
+    }
+
+}
