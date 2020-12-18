972adbccc1666df4019fca3124eb1af0bd10f6d9
Moa Stenqvist <moa@stonetwig.se>
2020-12-18 12:43:41 +0100

    resolved conflicts in sketch.

--- build/bundle.js
+++ build/bundle.js
@@ -126,11 +126,6 @@ function setup() {
     game = new TheGame();
 }
 function draw() {
-    background('blue');
-    fill('green');
-    stroke('white');
-    strokeWeight(10);
-    circle(width * .5, height * .5, width * 0.2);
     game.update();
     game.draw();
 }
