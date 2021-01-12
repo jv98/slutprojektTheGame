10355e1080d290ab9e35af7665c567422d671c5b
Oliver Nygren <oliver.nygren@icloud.com>
2021-01-12 10:51:51 +0100

    added interface for fonts and imgs. added to preload

--- src/endscenewin.ts
+++ src/endscenewin.ts
@@ -1,26 +1,13 @@
 class EndSceneWin {
-  private imgLogo: p5.Image;
-  private playerTrophy: p5.Image;
-  private poppinsBold: p5.Font;
-  private poppinsMedium: p5.Font;
-  private poppinsLight: p5.Font;
-
-  constructor() {
-      this.imgLogo = loadImage('./assets/logo-fs.png');
-      this.playerTrophy = loadImage('./assets/player-trophy.png');
-      this.poppinsBold = loadFont('./assets/poppins/Poppins-Bold.ttf');
-      this.poppinsMedium = loadFont('./assets/poppins/Poppins-Medium.ttf');
-      this.poppinsLight = loadFont('./assets/poppins/Poppins-Light.ttf');
-  }
 
   draw() {
     push();
     background(0);
     imageMode(CENTER)
-    image(this.imgLogo, 1250 / 2, 110, 350, 170)
-    image(this.playerTrophy, 1050, 400, 250, 250)
+    image(images.logo, 1250 / 2, 110, 350, 170)
+    image(images.playerTrophy, 1050, 400, 250, 250)
 
-    textFont(this.poppinsBold);
+    textFont(fonts.poppinsBold);
     fill(255);
 
     textSize(50);
@@ -28,18 +15,18 @@ class EndSceneWin {
     text('C O N G R A T U L A T I O N S !', 1250 / 2, 300);
     
     textSize(26);
-    textFont(this.poppinsMedium);
+    textFont(fonts.poppinsMedium);
     text('Willow is incredibly thankful for your help', 1250 / 2, 350);
-    textFont(this.poppinsLight)
+    textFont(fonts.poppinsLight)
 
     textSize(18);
     text('Thanks to YOU she was able to get her teddy bear back', 1250 / 2, 400);
-    textFont(this.poppinsBold)
+    textFont(fonts.poppinsBold)
 
     textSize(22);
     text('WANT TO PLAY AGAIN?', 1250 / 2, 475);
 
-    textFont(this.poppinsLight)
+    textFont(fonts.poppinsLight)
     text('Hit ENTER', 1250 / 2, 510);
     pop();
   }  
--- src/startmenu.ts
+++ src/startmenu.ts
@@ -1,30 +1,20 @@
 class StartMenu {
 
-  private imgLogo: p5.Image;
-  private poppinsBold: p5.Font;
-  private poppinsLight: p5.Font;
-
-  constructor() {
-      this.imgLogo = loadImage('./assets/logo-fs.png');
-      this.poppinsBold = loadFont('./assets/poppins/Poppins-Bold.ttf');
-      this.poppinsLight = loadFont('./assets/poppins/Poppins-Light.ttf');
-  }
-
   draw() {
     push()
       background(0);
       imageMode(CENTER)
-      image(this.imgLogo, 1250 / 2, 130, 350, 170)
+      image(images.logo, 1250 / 2, 130, 350, 170)
   
       // INSTRUCTIONS
-      textFont(this.poppinsBold);
+      textFont(fonts.poppinsBold);
       textSize(18);
       textAlign(CENTER);
       fill(255);
       text('I N S T R U C T I O N S', 1250 / 2, 250);
   
       //MOVE LEFT OR RIGHT
-      textFont(this.poppinsLight);
+      textFont(fonts.poppinsLight);
       textSize(18);
       textAlign(CENTER);
       fill(255);
@@ -32,28 +22,28 @@ class StartMenu {
       text('Move Left: Press A or <-\nMove Right: Press D or ->', 1250 / 2, 280);
   
       // BACKSTORY TITLE
-      textFont(this.poppinsBold);
+      textFont(fonts.poppinsBold);
       textSize(18);
       textAlign(CENTER);
       fill(255);
       text('B A C K S T O R Y', 1250 / 2, 365);
   
       // BACKSTORY TEXT
-      textFont(this.poppinsLight);
+      textFont(fonts.poppinsLight);
       textSize(18);
       textAlign(CENTER);
       fill(255);
       text('Little Willow has just lost her most priced possession, her favorite teddy bear.\nTo get the bear back, she has to catch the falling stars to make her wish come true.\n You have to help her catch as many stars as possible,\nbut look out for the nails!', 1250 / 2, 395);
   
       // READY TO PLAY
-      textFont(this.poppinsBold);
+      textFont(fonts.poppinsBold);
       textSize(22);
       textAlign(CENTER);
       fill(255);
       text('READY TO PLAY?', 1250 / 2, 515);
   
       // HIT ENTER TO START
-      textFont(this.poppinsLight);
+      textFont(fonts.poppinsLight);
       textSize(22);
       textAlign(CENTER);
       fill(255);
